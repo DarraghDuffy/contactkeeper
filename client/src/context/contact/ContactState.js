810c364857ab5bdeda9c847f75bacb42840edd19
darraghduffy <darraghduffy@me.com>
2020-04-16 23:05:59 +0100

    fix

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -37,12 +37,7 @@ const ContactState = (props) => {
       dispatch({ type: GET_CONTACTS, payload: res.data });
     } catch (error) {
       let errorObj = null;
-      if (error.response.data.errors) {
-        errorObj = error.response.data.errors;
-      } else {
-        errorObj = [{ msg: error.response.data.message }];
-      }
-      dispatch({ type: CONTACT_ERROR, payload: errorObj });
+      // } else {
     }
   };
 
