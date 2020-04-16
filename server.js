1b2c6f258dd7b145f89d3581ed7fbb762cd96eb2
darraghduffy <darraghduffy@me.com>
2020-04-16 23:00:00 +0100

    production

--- server.js
+++ server.js
@@ -10,9 +10,11 @@ connectDB();
 app.use(express.json({ extended: false }));
 app.use(cors());
 
+// Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  //set static folder
+  // Set static folder
   app.use(express.static('client/build'));
+
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
