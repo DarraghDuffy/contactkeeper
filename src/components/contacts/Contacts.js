755575c6cc99a63ead33c28c003545e4794f215f
darraghduffy <darraghduffy@me.com>
2020-04-16 16:30:19 +0100

    implemented backend contacts

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,21 +1,39 @@
-import React, { useContext, Fragment } from 'react';
+import React, { useContext, Fragment, useEffect } from 'react';
 import ContactContext from '../../context/contact/contactContext';
 import ContactItem from './ContactItem';
+import Spinner from './../layout/Spinner';
+import { GET_CONTACTS } from '../../context/types';
 
 export default function Contacts() {
   const contactContext = useContext(ContactContext);
-  const { contacts, filtered, filterSearch } = contactContext;
+  const {
+    contacts,
+    filtered,
+    filterSearch,
+    getContacts,
+    loading,
+  } = contactContext;
+
+  useEffect(() => {
+    getContacts();
+    //eslint-disable-next-line
+  }, []);
+
   return (
     <Fragment>
       {contacts.length > 0 &&
         !filtered &&
         filterSearch.length === 0 &&
         contacts.map((contact) => {
-          return <ContactItem key={contact.id} contact={contact}></ContactItem>;
+          return (
+            <ContactItem key={contact._id} contact={contact}></ContactItem>
+          );
         })}
       {filtered &&
         filtered.map((contact) => {
-          return <ContactItem key={contact.id} contact={contact}></ContactItem>;
+          return (
+            <ContactItem key={contact._id} contact={contact}></ContactItem>
+          );
         })}
       {filtered && filtered.length === 0 && filterSearch.length > 0 && (
         <p className='text-primary'>no contacts found</p>
