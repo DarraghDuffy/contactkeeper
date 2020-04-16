21e55cb55a88d1a4f1e46bb0fc01f350966ca1b4
darraghduffy <darraghduffy@me.com>
2020-04-16 22:27:43 +0100

    hmm production stuff

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,11 +1,10 @@
-// module.export = {
-//   baseURL: 'htp//
+// const baseURL = 'http://localhost:5000';
+// const users = baseURL + '/api/users';
+// const contacts = baseURL + '/api/contacts/';
+// const auth = baseURL + '/api/auth';
 
-// };
-
-const baseURL = 'http://localhost:5000';
-const users = baseURL + '/api/users';
-const contacts = baseURL + '/api/contacts/';
-const auth = baseURL + '/api/auth';
+const users = '/api/users';
+const contacts = '/api/contacts/';
+const auth = '/api/auth';
 
 export { users, contacts, auth };
