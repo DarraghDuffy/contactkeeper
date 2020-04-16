9fb1de746117fa36a17eb5ee7b2e894d1c3ec551
darraghduffy <darraghduffy@me.com>
2020-04-16 22:35:08 +0100

    damn production!

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,10 +1,8 @@
 // const baseURL = 'http://localhost:5000';
-// const users = baseURL + '/api/users';
-// const contacts = baseURL + '/api/contacts/';
-// const auth = baseURL + '/api/auth';
 
-const users = '/api/users';
-const contacts = '/api/contacts/';
-const auth = '/api/auth';
+const baseURL = 'https://darraghduffy-contactkeeper.herokuapp.com';
+const users = baseURL + '/api/users';
+const contacts = baseURL + '/api/contacts/';
+const auth = baseURL + '/api/auth';
 
 export { users, contacts, auth };
